1d3adf51f5e1e02a03cc87f2e4ad047d19798b4f
SilviaKlodi <silviaklodi@yahoo.com.ar>
2018-06-23 16:07:49 -0300

    Clases
    Clases
    clases

---
+++ src/remis/viaje.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package remis;
+
+/**
+ *
+ * @author u583313
+ */
+public class viaje {
+
+    private int idViaje;
+    private int origen;
+    private int destino;
+    private float kms;
+    private date fechaSalida;
+    private date fechaLlegada;
+    private float valor;
+    
+    public viaje altaViaje(){
+    
+    }
+    
+    public void consultarInformes(){
+        
+    }
+    
+    public void actualizarEstadoViaje(){
+}
+    
+}
